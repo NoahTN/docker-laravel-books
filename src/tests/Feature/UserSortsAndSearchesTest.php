85b00f74e190588ddef5bd56f8ea23b8dc9d17d3
NoahTN <ntolentino-nguyen@csumb.edu>
2023-10-01 15:11:46 -0700

    Update test case names

--- src/tests/Feature/UserSortsAndSearchesTest.php
+++ src/tests/Feature/UserSortsAndSearchesTest.php
@@ -8,28 +8,28 @@ use Tests\TestCase;
 
 class UserSortsAndSearchesTest extends TestCase
 {
-    public function test_filter_books_by_titles_with_matching_beginning_characters_sorted_alphabetically_by_title_ascending() 
+    public function test_filter_books_by_titles_or_authors_with_words_having_beginning_characters_matching_input_sorted_alphabetically_by_title_ascending() 
     {  
         $response = $this->get('/');
 
         $response->assertStatus(400);
     } 
 
-    public function test_filter_books_by_titles_with_matching_beginning_characters_sorted_alphabetically_by_title_descending() 
+    public function test_filter_books_by_titles_or_authors_with_words_having_beginning_characters_matching_input_sorted_alphabetically_by_title_descending() 
     {  
         $response = $this->get('/');
 
         $response->assertStatus(400);
     } 
 
-    public function test_filter_books_by_titles_with_matching_beginning_characters_sorted_alphabetically_by_author_ascending() 
+    public function test_filter_books_by_titles_or_authors_with_words_having_beginning_characters_matching_input_sorted_alphabetically_by_author_ascending() 
     {  
         $response = $this->get('/');
 
         $response->assertStatus(400);
     }
     
-    public function test_filter_books_by_titles_with_matching_beginning_characters_sorted_alphabetically_by_author_descending() 
+    public function test_filter_books_by_titles_or_authors_with_words_having_beginning_characters_matching_input_sorted_alphabetically_by_author_descending() 
     {  
         $response = $this->get('/');
 
