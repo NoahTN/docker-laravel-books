c2395918bf63f593491ec45f7751bdbc9b86faaa
NoahTN <ntolentino-nguyen@csumb.edu>
2023-10-01 17:21:05 -0700

    Add Export feature test file

---
+++ src/tests/Feature/UserExportTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserExportTest extends TestCase
+{
+    // test for each: Include Titles and Authors, Include Only Titles, Include only Authors
+    
+    public function test_export_csv_no_data() 
+    {
+      $this->assertTrue(false);
+    }
+
+    public function test_export_XML_no_data() 
+    {
+      $this->assertTrue(false);
+    }
+
+    public function test_export_CSV_with_data() 
+    {
+      $this->assertTrue(false);
+    }
+
+    public function test_export_XML_with_data() 
+    {
+      $this->assertTrue(false);
+    }
+}
